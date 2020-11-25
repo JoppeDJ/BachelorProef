591bee33c823c74938aef33b0938cc2220bc7b1a
juhacarlon <juha.carlon@student.kuleuven.be>
2020-11-26 00:18:51 +0100

    Verdergewerkt aan train functie.

---
+++ Code/hulpFuncties/train_net.m
@@ -0,0 +1,50 @@
+function trained_network = train_net(network, data, step_size, max_iterations, batch_size)
+% traint het netwerk gegeven de cell met de netwerk weights en biases, de
+% trainset, de stapgrootte, het aantal iteraties en de vatch grootte
+
+size = length(network) - 1;
+
+activations = cell(1, length(network)/2);
+deltas = cell(1, length(network)/2);
+
+%train loop
+for i = 1:max_iterations
+    indexes = randperm(length(data), batch_size);
+    gradient(indexes);   
+    
+    
+end
+
+% berekent de gradient van een batch, gegeven de indexen van de batchpunten
+
+function batch_gradient = gradient(indexes)
+    for index = 1:length(indexes)
+       current_point_index = indexes(index);
+       forward_pass(current_point_index);
+       backward_pass(current_point_index);
+       
+    end
+end
+
+% maakt een forward pass in het netwerk gegeven de index van de input
+    
+function forward_pass(point_index)
+    datapoint = [data(1,point_index),data(2,point_index)];
+    activations{1,1} = sigmoid_activate(datapoint', network{1,1}, network{1,2});
+    for index = 2:length(activations)
+        activations{1,index} = sigmoid_activate(activations{1,index - 1}, network{1,2*index - 1}, network{1,2*index});
+    end   
+end
+
+% maakt een backwards pass in het netwerk gegeven de index van de input
+% work in progress
+
+function backward_pass(point_index)
+    expectation = [data(3,point_index),data(4,point_index)];
+    deltas{1,end} = activations{1,end}.*(1-activations{1,end}).*(activations{1,end} - expectation);
+    for index = length(deltas):-1:2
+        deltas{1,index-1} = activations{1,index-1}.*(1-activations{1,index-1}).*(network{1,2*index - 1} - activations{1,index});
+    end
+end
+
+end
