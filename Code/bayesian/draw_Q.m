f975b3c8d4be98919d0d284f1194ca8b6d0a6132
Juha Carlon <juha.carlon@student.kuleuven.be>
2021-02-24 17:59:15 +0100

    Fixed MH algoritm and added uncertainty plotter.

--- Code/bayesian/draw_Q.m
+++ Code/bayesian/draw_Q.m
@@ -7,7 +7,7 @@ for i = 1:N1
    sz = size(network{1,i});
    for j = 1:sz(1,1)
       for k = 1:sz(1,2)
-          network{1,i}(j,k) = normrnd(network{1,i}(j,k), 0.01);
+          network{1,i}(j,k) = normrnd(network{1,i}(j,k), 0.5);
       end
    end
 end
--- Code/bayesian/metropolis_hasting.m
+++ Code/bayesian/metropolis_hasting.m
@@ -1,30 +1,36 @@
-function resultNetworks = metropolis_hasting()
+function resultNetworks = metropolis_hasting(iterations)
 %METROPOLIS_HASTING
 % data generation
 [data_XA, data_YA, data_XB, data_YB] = random_data_generator(1000,100,"Cirkel");
 data = [data_XA data_XB; data_YA data_YB; ones(1,length(data_XA)) zeros(1,length(data_XB)); zeros(1,length(data_XA)) ones(1,length(data_XB))];
 M = size(data);
 
-N = 1e6;
+N = iterations;
 xCell = cell(1,N);
 
 
 %draw initial
 xCell{1,1} = NN_gen([2 5 2],'normal',[0 10],5000);
 
-
+burn_count = 0;
 %Burn in
 i = 0;
-while i < 20000
+while i < 200
     k = randi(M(1,2));
     xt = draw_Q(xCell{1, 1});
     p = min([1;f(k,xt)/f(k,xCell{1,1})]);
     r = rand;
+    burn_count = burn_count + 1;
     if r <= p
       xCell{1,1} = xt;
       i = i + 1;
     end
 end
+
+burned = 200/burn_count;
+burned
+
+alg_count = 0;
 
 i = 1;
 while i < N
@@ -32,16 +38,31 @@ while i < N
     xt = draw_Q(xCell{1, i});
     p = min([1;f(k,xt)/f(k,xCell{1,i})]);
     r = rand;
+    alg_count = alg_count + 1;
     if r <= p
       xCell{1,i+1} = xt;
       i = i + 1;
     end
 end
 
+iterations  = N/alg_count;
+iterations
+
  function value = f(index, parameters)
-     exp_value = [data(3,index); data(4,index)];
-     data_point = [data(1,index); data(2,index)];
-     cost1 = exp(-0.5*(norm(exp_value - classify(data_point, parameters))/1)^2); 
+     
+%      exp_value = [data(3,index); data(4,index)];
+%      data_point = [data(1,index); data(2,index)];
+%      cost1 = exp(-0.5*((norm(exp_value - classify(data_point, parameters)))/10)^2); 
+     % All points f:
+     
+     cost1 = 1;
+     for ix = 1:length(data)
+         exp_value = [data(3,ix); data(4,ix)];
+         data_point = [data(1,ix); data(2,ix)];
+         cost1 = cost1 * exp(-0.5*(norm(exp_value - classify(data_point, parameters)))^2); 
+     end
+     
+
      cost2 = 1;
      for j = 1:length(parameters) 
          cost2 = cost2 * exp(-0.5*(norm(parameters{1,j})/10)^2);      
@@ -49,13 +70,16 @@ end
      value = cost1 * cost2;
  end
 
+resultNetworks = xCell;
 
-resultNetworks = cell(1,100);
+%resultNetworks = cell(1,100);
 %draw 100 samples to use for classification
-indexes = randperm(N,100);
-for ind = 1:10 
-    resultNetworks{1,ind} = xCell{1,indexes(ind)};
-end
+%indexes = randperm(N,100);
+% indexes = [N-99:1:N];
+% 
+% for ind = 1:10 
+%     resultNetworks{1,ind} = xCell{1,indexes(ind)};
+% end
 
 end
 
