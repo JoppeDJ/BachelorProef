971579cf9492f244b253e9478bbb6533948db5f4
Joppe <joppdj@gmail.com>
2021-03-05 15:46:32 +0100

    changes

--- Code/hulpFuncties/correct_classify_triangle.m
+++ Code/hulpFuncties/correct_classify_triangle.m
@@ -3,16 +3,16 @@ function classList = correct_classify_triangle(grid)
 classList = {};
 
 for index = 1:length(grid)
-    if grid(1,index) < 0.2 || grid(1,index) > 0.8 || grid(2,index) > 0.8 || grid(2,index) < 0.2
+    if grid(1,index) < 0.1 || grid(1,index) > 0.9 || grid(2,index) > 0.9 || grid(2,index) < 0.1
         classList = [classList, "B"];
     elseif grid(1,index) < 0.5
-        if grid(2,index) <= 2*grid(1,index) - 0.2
+        if grid(2,index) <= 2*grid(1,index) - 0.1
             classList = [classList, "A"];
         else
             classList = [classList, "B"];
         end
     elseif grid(1,index) >= 0.5
-        if grid(2,index) <= -2*grid(1,index) + 1.8
+        if grid(2,index) <= -2*grid(1,index) + 1.9
             classList = [classList, "A"];
         else
             classList = [classList, "B"];
